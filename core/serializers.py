1db5a96e7b8cafaddcc34d88c6845d7de825ce6b
KaueDeMoura <kaueoenningdmoura@gmail.com>
2022-08-25 14:23:53 -0300

    Marca Online

---
+++ core/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Categoria, Marca
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = "__all__"
+
+class MarcaSerializer(ModelSerializer):
+    class Meta:
+        model = Marca
+        fields = "__all__"
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria, Marca
+from core.serializers import CategoriaSerializer, MarcaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
